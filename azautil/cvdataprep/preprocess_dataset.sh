55ba45ad989fe716a1d50452809f1f9ac1187915
dhung1 <davidhung@email.arizona.edu>
2017-04-27 17:29:15 -0700

    Modified logging to include dataset sizes

--- azautil/cvdataprep/preprocess_dataset.sh
+++ azautil/cvdataprep/preprocess_dataset.sh
@@ -2,15 +2,15 @@
 
 DIR=${1%/}
 
-echo 'Calculating meanstd of dataset, saving result to' $DIR/meanstd.txt
-python calc_meanstd.py -d $DIR > $DIR/meanstd.txt
+echo 'Calculating meanstd of dataset, saving result to' $DIR/metadata.txt
+python calc_meanstd.py -d $DIR | tee $DIR/metadata.txt
 ALPHANUM_DIR=$DIR/preprocess_alphanums_out
 echo 'Creating preprocessed alphanumerics copy of dataset in' $ALPHANUM_DIR 
 python preprocess_alphanums.py -d $DIR
 
 # Main dataset
 echo 'Sampling dataset in' $DIR
-python sample_dataset.py -d $DIR
+python sample_dataset.py -d $DIR | tee -a $DIR/metadata.txt
 echo 'Converting data in' $DIR 'to TFRecords in' $DIR/TFRecords
 python json_to_tfrecord.py --train_directory $DIR/train_data --validation_directory $DIR/validation_data --output_directory $DIR/TFRecords --train_shards 128 --validation_shards 32
 
