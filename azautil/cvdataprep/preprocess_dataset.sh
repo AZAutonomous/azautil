23f98983cd7666d491767a5eedf982e19b6841c2
dhung1 <davidhung@email.arizona.edu>
2017-05-06 00:18:44 -0700

    Modified keep probability as temporary patch to bad sampling

--- azautil/cvdataprep/preprocess_dataset.sh
+++ azautil/cvdataprep/preprocess_dataset.sh
@@ -10,12 +10,12 @@ python preprocess_alphanums.py -d $DIR
 
 # Main dataset
 echo 'Sampling dataset in' $DIR
-python sample_dataset.py -d $DIR | tee -a $DIR/metadata.txt
+python sample_dataset.py -d $DIR -p .99 | tee -a $DIR/metadata.txt
 echo 'Converting data in' $DIR 'to TFRecords in' $DIR/TFRecords
 python json_to_tfrecord.py --train_directory $DIR/train_data --validation_directory $DIR/validation_data --output_directory $DIR/TFRecords --train_shards 128 --validation_shards 32
 
 # Alphanums dataset
 echo 'Sampling dataset in' $ALPHANUM_DIR
-python sample_dataset.py -d $ALPHANUM_DIR
+python sample_dataset.py -d $ALPHANUM_DIR -p .99
 echo 'Converting data in' $ALPHANUM_DIR 'to TFRecords in' $ALPHANUM_DIR/TFRecords
 python json_to_tfrecord.py --train_directory $ALPHANUM_DIR/train_data --validation_directory $ALPHANUM_DIR/validation_data --output_directory $ALPHANUM_DIR/TFRecords --train_shards 128 --validation_shards 32
